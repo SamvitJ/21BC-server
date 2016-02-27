cc194e5ce794b273df785fb96dc2505d8bdd71d1
SamvitJ <samvit.jain@gmail.com>
2016-02-27 17:11:21 +0000

    Updated flask server - added payment channels endpoint, specified port.

--- flask-server.py
+++ flask-server.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask, request
 
 ## Import from the Bitcoin library
@@ -16,24 +17,31 @@ def enable_cors(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+    print("RESPONSE HEADERS ---------")
+    print(response.headers)
     return response
 
 ## Default: free access
 @app.route('/')
 def endpoint1():
-    print (request.headers)
+    print("REQUEST HEADERS ----------")
+    print(request.headers)
     return "Welcome to the free access endpoint\n"
-    # return json.dumps({"message": "Welcome to the free access endpoint\n"})
 
 ## Alternative: payable endpoint
 @app.route('/payable')
-@payment.required(1000)
+@payment.required(100)
 def endpoint2():
-    print (request.headers)
+    print("REQUEST HEADERS ----------")
+    print(request.headers)
     return "Welcome to the payable endpoint\n"
-    # return json.dumps({"message": "Welcome to the payable endpoint\n"})
+
+@app.route('/current-temperature')
+@payment.required(50)
+def current_temperature():
+    return 'Probably about 65 degrees Fahrenheit.'
 
 ## Initialize and run the server
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
     # app.run(host='10.8.235.166', port=80)
